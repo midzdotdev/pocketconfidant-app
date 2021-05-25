326ebb664ddb72845eb638dec5d5b7e932fcf2d3
James Middleton <accounts@jamesmiddleton.me>
2021-05-25 21:30:40 +0100

    feat: add mock backend client

---
+++ api.ts
@@ -0,0 +1,47 @@
+import { ChatRoom, User } from "./types";
+
+export const fetchChatRooms = async (): Promise<ChatRoom[]> => {
+  return chatRooms;
+};
+
+const users: User[] = [
+  {
+    id: "user1",
+    forename: "James",
+    surname: "Middleton",
+    avatarUrl: "https://i.pravatar.cc/150?img=5",
+  },
+  {
+    id: "user2",
+    forename: "Olivier",
+    surname: "Malafronte",
+    avatarUrl: "https://i.pravatar.cc/150?img=10",
+  },
+  {
+    id: "user3",
+    forename: "Francesco",
+    surname: "Zangaro",
+    avatarUrl: "https://i.pravatar.cc/150?img=15",
+  },
+];
+
+const chatRooms: ChatRoom[] = [
+  {
+    id: "1",
+    users: [users[0], users[1]],
+    messages: [
+      {
+        id: "msg1",
+        author: users[0],
+        body: "This is a test",
+        timestamp: Date.now() - 3600 * 1000,
+      },
+      {
+        id: "msg2",
+        author: users[1],
+        body: "It works!",
+        timestamp: Date.now() - 60 * 1000,
+      },
+    ],
+  },
+];
